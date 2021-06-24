93fe7852eb39f2389e1451e5a7439097f5ffef8f
Taiyou Matsumoto <230373g@ws.kochi-tech.ac.jp>
2021-06-24 15:10:24 +0900

    add spectrogram

--- snd4/s1.m
+++ snd4/s1.m
@@ -3,13 +3,16 @@
 %音の切り替えるテンポを調節して比較する
 %音の周波数差を変えて比較する
 
+%脳は「一つの音源であれば短時間内に周波数成分が大きく変化する可能性は低い」
+%と考えるため、周波数が離れた音は別の音源として知覚される
+
 clear;
 
 Fs=16000;
 t=[0:Fs/5-1]/Fs;
 f=440;
-f1=880;
-f2=1500;
+f1=550;
+f2=2000;
 
 y=sin(2*pi*f*t);
 y1=sin(2*pi*f1*t);
@@ -19,9 +22,38 @@ b=zeros(1,Fs/5);
 Y1=[y y1 y b];
 Y2=[y y2 y b];
 
-Y1=[Y1 Y1 Y1 Y1];
-Y2=[Y2 Y2 Y2 Y2];
+Y1frq = fft(Y1);
+Y1frq = abs(Y1frq);
+Y1frq = fftshift(Y1frq);
 
+T = length(Y1)/Fs;         
+fax = [-Fs/2 : 1/T : Fs/2-1];
 
-sound(Y1,Fs);
+%spectrum
+%{
+figure;
+plot(fax, Y1frq);
+xlim([-1500 1500]);
+%}
 
+Y1=[Y1 Y1 Y1 Y1];   %音脈分凝を生じにくい
+Y2=[Y2 Y2 Y2 Y2];   %音脈分凝を生じやすい
+
+%time
+%{
+figure;
+tt = [0:3.2*Fs-1]/Fs;
+plot(tt, Y1);
+xlim([0 1.6]);
+%}
+
+figure;
+pspectrum(Y1, Fs, 'spectrogram', 'OverlapPercent', 0, 'Leakage', 1, 'MinThreshold', -60);
+title("440-880Hz, 音脈分凝を生じにくい音");
+
+figure;
+pspectrum(Y2, Fs, 'spectrogram', 'OverlapPercent', 0, 'Leakage', 1, 'MinThreshold', -60);
+title("440-1320Hz, 音脈分凝を生じやすい音");
+
+%sound(Y1,Fs);
+sound(Y2,Fs);
--- snd4/s3.m
+++ snd4/s3.m
@@ -1,6 +1,8 @@
 %%%%%%ミッシングファンダメンタル%%%%%%%%%%%%%%%%%%%%
-
-
+%基本周波数成分が含まれていなくても、残っている成分から推定され、
+%基本周波数に相当する高さが知覚される現象
+%脳が残っている倍音成分から基本周波数を推定するため、同じような音が知覚される
+%ミッシングファンダメンタルの音は同じ音階に聞こえるが、少し高い音のように感じる
 
 %%%%%%%%%%%%%%%%%%%%%%%%%矩形波%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -21,20 +23,30 @@ end
 y2=y1-sin(2*pi*f*t);
 
 figure;
-
-subplot(2,1,1);
-plot(t,y1);
-axis([0 0.01,-1 1]);
+plot(t, y1, t, y2);
+axis([0 0.005,-2 2]);
 xlabel('Time[s]');
 ylabel('Amplitude');
-title('矩形波');
+legend("矩形波", "missing fundamental");
+title('矩形波とミッシングファンダメンタル');
 
+figure;
+subplot(2,1,1);
+pspectrum(y1, Fs, 'spectrogram', 'OverlapPercent', 0, 'Leakage', 1, 'MinThreshold', -60);
+title("矩形波");
+
+subplot(2,1,2);
+pspectrum(y2, Fs, 'spectrogram', 'OverlapPercent', 0, 'Leakage', 1, 'MinThreshold', -60);
+title("missingfundamental");
+
+%{
 subplot(2,1,2);
 plot(t,y2);
 axis([0 0.01,-1 1]);
 xlabel('Time[s]');
 ylabel('Amplitude');
 title('矩形波-基本周波数');
+%}
 
 %sound(y1,Fs);  %ただの矩形波
 %sound(y2,Fs);  %矩形波から基本周波数を抜いた音(ミッシングファンダメンタル)
@@ -57,20 +69,21 @@ end
 y4=y3-sin(2*pi*f*t);
   
 figure;
-
-subplot(2,1,1);
-plot(t,y3);
-axis([0 0.01,-2.5 2.5]);
+plot(t,y3,t,y4);
+axis([0 0.005,-3 3]);
 xlabel('Time[s]');
 ylabel('Amplitude');
-title('ノコギリ波');
+legend("ノコギリ波", "missing fundamental");
+title('ノコギリ波とミッシングファンダメンタル');
+
+figure;
+subplot(2,1,1);
+pspectrum(y3, Fs, 'spectrogram', 'OverlapPercent', 0, 'Leakage', 1, 'MinThreshold', -60);
+title("ノコギリ波");
 
 subplot(2,1,2);
-plot(t,y4);
-axis([0 0.01,-2.5 2.5]);
-xlabel('Time[s]');
-ylabel('Amplitude');
-title('ノコギリ波-基本周波数');
+pspectrum(y4, Fs, 'spectrogram', 'OverlapPercent', 0, 'Leakage', 1, 'MinThreshold', -60);
+title("missingfundamental");
 
 %sound(y3,Fs);%ただのノコギリ波
 %sound(y4,Fs);%ノコギリ波から基本周波数を抜いた音(ミッシングファンダメンタル)
