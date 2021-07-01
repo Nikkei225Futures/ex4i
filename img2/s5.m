819d45fe6c7c3ebc8d15db758d87ed567573e983
Taiyou Matsumoto <230373g@ws.kochi-tech.ac.jp>
2021-07-01 16:17:27 +0900

    change method to apply filter

--- img2/s5.m
+++ img2/s5.m
@@ -6,26 +6,27 @@ faceImg = imread('thispersondoesnotexist.jpg');
 %rgb -> hsv
 faceImg = rgb2hsv(faceImg);
 
-%make filter
+%make logical filter
 hueFilter = faceImg(:,:,1) < 0.10;   %色相
-hueFilter = hueFilter > 0.01;
+hueFilter = hueFilter > 0.05;
 saturationFilter = faceImg(:,:,2) > 0.2;   %彩度
 brightnessFilter = faceImg(:,:,3) > 0.1;   %明度
 
 filter = hueFilter & saturationFilter & brightnessFilter;
-figure;
-imshow(filter);
 
-%apply filter
-brightness = faceImg(:,:,3);
-brightness(~(filter)) = 0;
-faceImg(:,:,3) = brightness;
+faceImg = hsv2rgb(faceImg); %hsv -> rgb
 
-%hsv -> rgb
-faceImg = hsv2rgb(faceImg);
+%apply filter to each color
+faceImg(:,:,1) = faceImg(:,:,1) .* filter;
+faceImg(:,:,2) = faceImg(:,:,2) .* filter;
+faceImg(:,:,3) = faceImg(:,:,3) .* filter;
 
 figure;
 imshow(faceImg);
 imwrite(faceImg,'skinFiltered.bmp');
+
+%figure;
+%imshow(filter);
+
 
 
