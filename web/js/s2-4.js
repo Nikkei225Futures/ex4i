a63ad610127592987a18bc618ea6a8604eeb5140
Taiyou Matsumoto <230373g@ws.kochi-tech.ac.jp>
2021-07-12 13:03:13 +0900

    add next

--- web/js/s2-4.js
+++ web/js/s2-4.js
@@ -1,6 +1,15 @@
-var target = document.getElementById("list");
-
-
-function click(){
+function getTime() {
+    var target = document.getElementById("list");
     var newList = document.createElement("li");
+    newList.innerHTML = new Date();
+    target.appendChild(newList);
+    
+}
+
+function clickHere() {
+    var target = document.getElementById("list");
+    var newList = document.createElement("li");
+    newList.innerHTML = "clickHere";
+    target.appendChild(newList);
+
 }
