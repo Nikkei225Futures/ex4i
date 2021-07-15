1e9ae28b08bde55a6cbb366a523b0af7e5ed20cf
Taiyou Matsumoto <230373g@ws.kochi-tech.ac.jp>
2021-07-15 13:37:22 +0900

    add next

--- web/js/s2-5.js
+++ web/js/s2-5.js
@@ -6,29 +6,10 @@ function getTime() {
     var min = dateTime.getMinutes();
     var sec = dateTime.getSeconds();
     var str = document.createTextNode(hour + "時" + min + "分" + sec + "秒");
-    
     newList.appendChild(str);
     target.appendChild(newList);
+    console.log(hour + ":" + min + ":" + sec);
     
-    invSortList();
-}
-
-function invSortList(){
-    var parent = document.getElementById("list");
-    var children = parent.children;
-    //console.log(children[children.length-1].innerHTML);
     
-    //inverse sort
-
-    let i = 0;
-    let j = children.length-1;
-
-    for(k = 0; k < (children.length-1)/2; k++){
-        temp = children[i].innerHTML;
-        children[i].innerHTML = children[j].innerHTML;
-        children[j].innerHTML = temp;
-        i++;
-        j--;
-    }
 
 }
